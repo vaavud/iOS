e59502dfc0d0701dcdbecfd9f6e92a773e96727f
Andreas Bruun Okholm <aokholm@gmail.com>
2013-05-08 17:33:26 +0200

    Added MagneticFieldSensor

---
+++ VaavudCore/Constants.h
@@ -0,0 +1,9 @@
+//
+//  Constants.h
+//  VaavudCore
+//
+//  Created by Andreas Okholm on 5/8/13.
+//  Copyright (c) 2013 Andreas Okholm. All rights reserved.
+//
+
+#define preferedSampleFrequency 60
