d235fb1b29cd7af73d95eb2806ad77d3c896c4a2
Thomas Stilling Ambus <thomas@ambus.dk>
2014-02-04 11:00:03 +0100

    Ongoing work on history.

---
+++ VaavudCore/MeasureNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  MeasureNavigationController.h
+//  Vaavud
+//
+//  Created by Thomas Stilling Ambus on 31/01/2014.
+//  Copyright (c) 2014 Andreas Okholm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeasureNavigationController : UINavigationController
+
+@end
---
+++ VaavudCore/MeasureNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  MeasureNavigationController.m
+//  Vaavud
+//
+//  Created by Thomas Stilling Ambus on 31/01/2014.
+//  Copyright (c) 2014 Andreas Okholm. All rights reserved.
+//
+
+#import "MeasureNavigationController.h"
+
+@interface MeasureNavigationController ()
+
+@end
+
+@implementation MeasureNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
