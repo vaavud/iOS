19f0caab65eed543df58f9af878b3fbe71a2954a
Gustaf Kugelberg <kugel@kth.se>
2015-01-27 22:45:45 +0100

    New features flow

--- VaavudCore/UpgradingUserViewController.swift
+++ VaavudCore/UpgradingUserViewController.swift
@@ -12,26 +12,41 @@ class UpgradingUserViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pager: UIPageControl!
     
+    @IBOutlet weak var phoneOffset: NSLayoutConstraint!
+    @IBOutlet weak var phoneScrollView: UIScrollView!
+    
     let pageCount = 3
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
         scrollView.contentSize = CGSize(width: CGFloat(pageCount)*scrollView.bounds.width, height: scrollView.bounds.height)
         
-//        let vc = UIStoryboard(name: "MainStoryboard", bundle: nil).instantiateViewControllerWithIdentifier("UpgradingUserPages") as UIViewController
-//        
-//        presentViewController(vc, animated: false, completion: nil)
-//        vc.view.frame = scrollView.bounds
-//        
-//        scrollView.addSubview(vc.view)
-        
-        if let v = NSBundle.mainBundle().loadNibNamed("UpgradingUserPagesView", owner: nil, options: nil).first as? UIView {
-            v.frame.size = scrollView.contentSize
-            scrollView.addSubview(v)
+        if let content = NSBundle.mainBundle().loadNibNamed("UpgradingUserPagesView", owner: nil, options: nil).first as? UIView {
+            content.frame.size = scrollView.contentSize
+            scrollView.addSubview(content)
         }
         
+        phoneScrollView.contentSize = CGSize(width: 2*phoneScrollView.bounds.width, height: phoneScrollView.bounds.height)
+
+        if let content = NSBundle.mainBundle().loadNibNamed("UpgradingUserPhonePages", owner: nil, options: nil).first as? UIView {
+            content.frame.size = phoneScrollView.contentSize
+            phoneScrollView.addSubview(content)
+        }
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
-        pager.currentPage = Int(round(scrollView.contentOffset.x/scrollView.bounds.width))
+        let page = scrollView.contentOffset.x/scrollView.bounds.width
+        pager.currentPage = Int(round(page))
+        
+        if page > 1 {
+            phoneOffset.constant = scrollView.bounds.width - scrollView.contentOffset.x
+        }
+        else {
+            phoneScrollView.contentOffset.x = page*phoneScrollView.bounds.width
+        }
     }
 }
