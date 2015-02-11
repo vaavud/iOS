572f673f751f6f75629ce423925540d6cd198511
Gustaf Kugelberg <kugel@kth.se>
2015-02-11 16:13:52 +0100

    build error

---
+++ VaavudCore/CoreSettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CoreSettingsViewController.swift
+//  Vaavud
+//
+//  Created by Gustaf Kugelberg on 11/02/15.
+//  Copyright (c) 2015 Andreas Okholm. All rights reserved.
+//
+
+import Foundation
+
+class CoreSettingsTableViewController: UITableViewController {
+    
+}
