5f14629a171ba71e25bb349c789daa849b007483
GreatApe <kugel@kth.se>
2015-10-15 14:00:25 +0200

    removed duplicate

--- VaavudCore/MapViewController.m
+++ VaavudCore/MapViewController.m
@@ -408,10 +408,6 @@
     return UIStatusBarStyleLightContent;
 }
 
--(void)mapViewDidFinishRenderingMap:(MKMapView *)mapView fullyRendered:(BOOL)fullyRendered {
-    [self refreshMap];
-}
-
 -(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
     if (control.enabled) {
         [self performSegueWithIdentifier:@"ForecastSegue" sender:view.annotation];
